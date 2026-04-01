1f88b7d30007ab05ab94d1ce9139d002640e5cf8
Mark Masterson <markmasterson@gmail.com>
2026-04-01 18:26:17 -0500

    Format wasm smoke test for rustfmt compliance

--- autumn-wasm/tests/wasm_smoke.rs
+++ autumn-wasm/tests/wasm_smoke.rs
@@ -7,6 +7,10 @@ fn noop_mount(_: autumn_wasm::Element, _: String) {}
 
 #[wasm_bindgen_test]
 fn boot_accepts_registry_on_wasm_target() {
-    let registry = [IslandRegistration::new("counter", "counter-root", noop_mount)];
+    let registry = [IslandRegistration::new(
+        "counter",
+        "counter-root",
+        noop_mount,
+    )];
     boot(&registry);
 }
