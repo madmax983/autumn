8de90776efe70353d3e2c265f95f8e3f2b188029
Mark Masterson <markmasterson@gmail.com>
2026-04-09 08:02:53 -0500

    fix: avoid harvest migration version collisions

--- examples/reddit-clone/src/main.rs
+++ examples/reddit-clone/src/main.rs
@@ -33,7 +33,6 @@ mod tasks;
 mod workflows;
 
 use autumn_harvest::prelude::WorkerConfig;
-use autumn_web_harvest::prelude::HarvestExt;
 use autumn_web::migrate::{EmbeddedMigrations, embed_migrations};
 use autumn_web::prelude::*;
 use autumn_web_harvest::prelude::HarvestExt;
