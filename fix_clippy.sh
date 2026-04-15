55c40bda3533387c745088ab49d36124d62f0cbc
Mark Masterson <markmasterson@gmail.com>
2026-04-15 13:53:08 -0500

    Delete fix_clippy.sh

--- fix_clippy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sed -i '/use autumn_harvest::models::NewWorkflowExecution;/d' autumn-harvest/autumn-harvest/tests/signal_tests.rs
-sed -i '/use autumn_harvest::schema::harvest_workflow_executions;/d' autumn-harvest/autumn-harvest/tests/signal_tests.rs
-sed -i '3i use autumn_harvest::models::NewWorkflowExecution;\nuse autumn_harvest::schema::harvest_workflow_executions;' autumn-harvest/autumn-harvest/tests/signal_tests.rs
